bf4b782dd710866e4e12aafc6d41985d37844ec2
MarcioViana <92828499+ifamarcio@users.noreply.github.com>
2021-11-11 07:32:15 -0400

    adicionei peimeiros codigos

---
+++ CursoJava/src/cursojava/Fundamentos.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author marcio
+ *
+ */
+public class Fundamentos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String nome ;
+		int idade = 30;
+		char sexo = 'M';
+		double temperatura = 26.7;
+		boolean arcondicionado = false;
+		nome = "Márcio Viana";
+		System.out.println("uso de variaveis na linguagem java");
+		System.out.println("exempos:");
+		System.out.println("Nome:" + nome);
+		System.out.println("Idade:"+ idade);
+		System.out.println("Sexo"+ sexo);
+		System.out.println("Temperatura"+ temperatura);
+		System.out.println("Arcondicionado"+ arcondicionado);
+		System.out.println("_________________________________");
+		System.out.println("");
+		double i = 10;
+		System.out.println("operadores aritimeticos e seus atributos");
+		System.out.println("exemplos:");
+		System.out.println("i = "+);
+	}
+
+}
---
+++ CursoJava/src/cursojava/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cursojava;
+
+/**
+ * @author marcio
+ * Hello World
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World"); 
+
+	}
+
+}
